47e1f57895876e3094767053401e69aa15b7f5f1
Alexandru Pisarenco <pisarenco.a@gmail.com>
2020-05-04 02:29:03 +0200

    Working solution

--- etl.py
+++ etl.py
@@ -1,20 +1,35 @@
 import contextlib
-from datetime import datetime
+from datetime import date
 from functools import namedtuple
 import psycopg2
-from typing import List, Union
+from typing import Dict, List, Union
+
+
+def get_connect_arguments() -> Dict[str, str]:
+    dsn = dict()
+    try:
+        import config
+    except ImportError:
+        return dsn
+
+    if hasattr(config, "db") and isinstance(config.db, dict):
+        dsn.update(config.db)
+    return dsn
 
 
 @contextlib.contextmanager
 def connect(db: str):
     conn, cursor = None, None
     try:
-        conn = psycopg2.connect(f"dbname={db}")
+        dsn = get_connect_arguments()
+        dsn["dbname"] = db
+        conn = psycopg2.connect(**dsn)
         cursor = conn.cursor()
         yield conn, cursor
     finally:
+        if conn:
+            conn.commit()
         if cursor:
-            cursor.commit()
             cursor.close()
         if conn:
             conn.close()
@@ -44,7 +59,7 @@ def get_columns(rel: Relation):
 def make_sure_table_exists(rel: Relation, columns: List[Column], key):
     with connect(rel.db) as (_, curs):
         cols_ddl = ',\n                         '.join(
-            [f'"{col.name}" {col.type} {"UNIQUE" if key==col.name else ""}' for col in columns]
+            [f'"{col.name}" {col.type}{" UNIQUE" if key==col.name else ""}' for col in columns]
         )
         curs.execute(f"""
                      CREATE TABLE IF NOT EXISTS "{rel.schema}"."{rel.name}" (
@@ -53,24 +68,25 @@ def make_sure_table_exists(rel: Relation, columns: List[Column], key):
                      """)
 
 
-def get_last_uploaded_time(rel: Relation, log: Relation) -> Union[datetime, None]:
+def get_last_uploaded_time(rel: Relation, log: Relation) -> Union[date, None]:
     with connect(log.db) as (_, curs):
         curs.execute(f"""
                      SELECT loaded
                      FROM "{log.schema}"."{log.name}"
-                     WHERE schema_name=%s AND relation_name=%s
+                     WHERE schema_name=%s AND relation_name=%s;
                      """, (rel.schema, rel.name))
         row = curs.fetchone()
         if row:
             return row[0]
 
 
-def copy(src: Relation, tgt: Relation, key_field: str, tstz_field: str, last_upload: datetime):
+def copy(src: Relation, tgt: Relation, key_field: str, tstz_field: str, last_upload: date):
     with connect(src.db) as (_, curs):
+        # Since the starting date for the extraction can be null, daterange is used as it treats NULL as "no limit"
         curs.execute(f"""
                      SELECT *
                      FROM "{src.schema}"."{src.name}"
-                     WHERE "{tstz_field}" <@ TSTZRANGE(NULL, %s, '()')
+                     WHERE "{tstz_field}" <@ DATERANGE(%s::DATE, NULL, '[)');
                      """, (last_upload, ))
 
         columns = '", "'.join(desc[0] for desc in curs.description)
@@ -80,22 +96,23 @@ def copy(src: Relation, tgt: Relation, key_field: str, tstz_field: str, last_upl
         INSERT INTO "{tgt.schema}"."{tgt.name}"
         ("{columns}")
         VALUES ({placeholders})
+        ON CONFLICT ("{key_field}") DO NOTHING;
         """
         with connect(tgt.db) as (_, tgt_curs):
             tgt_curs.executemany(insert_query, curs.fetchall())
 
 
-def log_upload(rel: Relation, log: Relation, last_upload: datetime):
+def log_upload(rel: Relation, log: Relation, last_upload: date):
     with connect(log.db) as (_, curs):
         curs.execute(f"""
                      DELETE FROM "{log.schema}"."{log.name}"
-                     WHERE schema_name=%s AND relation_name=%s
+                     WHERE schema_name=%s AND relation_name=%s;
                      """, (rel.schema, rel.name))
         curs.execute(f"""
                      INSERT INTO "{log.schema}"."{log.name}"
                      (schema_name, relation_name, loaded)
                      VALUES
-                     (%s, %s, %s)
+                     (%s, %s, %s);
                      """, (rel.schema, rel.name, last_upload))
 
 
@@ -107,6 +124,8 @@ def main():
         Column("loaded", "TIMESTAMPTZ")
     ]
 
+    print("Getting source data schema")
+
     source_address = Relation("source", "public", "address")
     target_address = Relation("target", "public", "address")
 
@@ -115,21 +134,29 @@ def main():
     source_company = Relation("source", "public", "company")
     target_company = Relation("target", "public", "company")
 
-    company_columns = get_columns(source_address)
+    company_columns = get_columns(source_company)
 
-    make_sure_table_exists(address_columns, target_address, "id")
-    make_sure_table_exists(company_columns, target_company, "company_Id")
-    make_sure_table_exists(log_columns, log)
+    print("Ensuring schema at destination")
+
+    make_sure_table_exists(target_address, address_columns, "id")
+    make_sure_table_exists(target_company, company_columns, "company_id")
+    make_sure_table_exists(log, log_columns, None)
+    
+    print("Copying data")
 
     last_upload = get_last_uploaded_time(target_address, log)
-    now = datetime.now()
+    print(f"Getting address data since {last_upload}")
+    today = date.today()
     copy(source_address, target_address, "id", "created_at", last_upload)
-    log_upload(target_address, log, now)
+    log_upload(target_address, log, today)
 
     last_upload = get_last_uploaded_time(target_company, log)
-    now = datetime.now()
+    print(f"Getting company data since {last_upload}")
+    today = date.today()
     copy(source_company, target_company, "company_id", "created_at", last_upload)
-    log_upload(target_address, log, now)
+    log_upload(target_company, log, today)
+
+    print("Done")
 
 
 if __name__=='__main__':
--- test.py
+++ test.py
@@ -1,9 +1,13 @@
+import config
 import etl
 import psycopg2
 import pytest
 import re
-from unittest.mock import MagicMock, Mock, patch
+from unittest.mock import MagicMock, Mock, PropertyMock, patch, call
 
+
+# Keep it simple, make sure our tests don't rely on a config
+config.db = {}
 
 whitespace_re = re.compile(r'[\s\n\r]+', re.MULTILINE)
 def canonicalize(val):
@@ -16,12 +20,13 @@ def get_mock_cursor():
     cursor = Mock()
     cursor.execute=MagicMock()
     cursor.fetchall=MagicMock(return_value=[])
-    cursor.commit=MagicMock()
     cursor.close=MagicMock()
     return cursor
 
 
-def get_mock_context(cursor):
+def mock_connection(cursor):
+    """ Replace the context-manager-enabled connect() function
+    """
     ctx = Mock()
     ctx.__enter__ = MagicMock(return_value=(Mock(), cursor))
     ctx.__exit__ = MagicMock()
@@ -32,13 +37,14 @@ def get_mock_context(cursor):
 
 @pytest.fixture
 def mock_psycopg2():
-    def conn(dsn):
-        if dsn=='dbname=error':
+    def conn(**kwargs):
+        if kwargs.get("dbname")=='error':
             raise Exception()
         else:
             connection = Mock()
             connection.cursor = MagicMock(return_value=get_mock_cursor())
             connection.close = MagicMock()
+            connection.commit = MagicMock()
             return connection
 
     psycopg2.connect = Mock()
@@ -49,8 +55,8 @@ def mock_psycopg2():
 
 @pytest.mark.parametrize("db, dsn, error",
                          [
-                             ("test", "dbname=test", False),
-                             ("error", "dbname=error", True)
+                             ("test", "test", False),
+                             ("error", "error", True)
                          ])
 def test_connect(mock_psycopg2, db, dsn, error):
     if error:
@@ -61,9 +67,9 @@ def test_connect(mock_psycopg2, db, dsn, error):
                 assert curs is None
     else:
         with etl.connect(db) as (conn, curs):
-            mock_psycopg2.connect.assert_called_once_with(dsn)
+            mock_psycopg2.connect.assert_called_once_with(dbname=dsn)
             conn.cursor.assert_called_once_with()
-        curs.commit.assert_called_once_with()
+        conn.commit.assert_called_once_with()
         curs.close.assert_called_once_with()
         conn.close.assert_called_once_with()
 
@@ -71,7 +77,7 @@ def test_connect(mock_psycopg2, db, dsn, error):
 def test_get_columns():
     cursor = get_mock_cursor()
 
-    ctx = get_mock_context(cursor)
+    ctx = mock_connection(cursor)
 
     result = etl.get_columns(etl.Relation("test", "stest", "ttest"))
 
@@ -86,7 +92,7 @@ def test_get_columns():
 def test_make_sure_table_exists():
     cursor = get_mock_cursor()
 
-    ctx = get_mock_context(cursor)
+    ctx = mock_connection(cursor)
 
     result = etl.make_sure_table_exists(
         etl.Relation("test", "public", "mock"),
@@ -103,7 +109,111 @@ def test_make_sure_table_exists():
     cursor.execute.assert_called_once()
     assert len(cursor.execute.call_args.args) == 1
     assert canonicalize(
-        'CREATE TABLE IF NOT EXISTS "public"."mock" ( "test" text unique, "test2" integer );'
+            'CREATE TABLE IF NOT EXISTS "public"."mock" ( "test" text unique, "test2" integer );'
         ) == canonicalize(
             cursor.execute.call_args.args[0]
         )
+
+
+def test_get_last_uploaded_time():
+    cursor = get_mock_cursor()
+    cursor.fetchone=MagicMock(return_value=[123])
+
+    ctx = mock_connection(cursor)
+
+    result = etl.get_last_uploaded_time(
+        etl.Relation("foo", "bar", "baz"),
+        etl.Relation("test", "public", "log_mock")
+    )
+
+    ctx.__enter__.assert_called_once()
+
+    etl.connect.assert_called_once_with("test")
+    cursor.execute.assert_called_once()
+    assert len(cursor.execute.call_args.args) == 2
+    assert canonicalize(
+            '''SELECT loaded FROM "public"."log_mock" WHERE schema_name=%s AND relation_name=%s;'''
+        ) == canonicalize(
+            cursor.execute.call_args.args[0]
+        )
+    assert cursor.execute.call_args.args[1] == ('bar', 'baz')
+    cursor.fetchone.assert_called_once()
+    assert result == 123
+
+
+def test_copy():
+    cursor = get_mock_cursor()
+    type(cursor).description=PropertyMock(return_value=[("foo_column",)])
+    cursor.executemany=MagicMock()
+    data = [("mock_value",)]
+    cursor.fetchall=MagicMock(return_value=data)
+
+    ctx = mock_connection(cursor)
+
+    result = etl.copy(
+        etl.Relation("src", "src_sch", "src_rel"),
+        etl.Relation("tgt", "tgt_sch", "tgt_rel"),
+        "mock_id",
+        "mock_tstz_field",
+        "mock_datetime"
+    )
+
+    ctx.__enter__.assert_has_calls([call(), call()])
+
+    etl.connect.assert_has_calls([call("src"), call("tgt")])
+    cursor.execute.assert_called_once()
+    assert len(cursor.execute.call_args.args) == 2
+    assert canonicalize(
+            '''SELECT * FROM "src_sch"."src_rel" WHERE "mock_tstz_field" <@ DATERANGE(%s::DATE, NULL, '[)');'''
+        ) == canonicalize(
+            cursor.execute.call_args.args[0]
+        )
+    assert cursor.execute.call_args.args[1] == ("mock_datetime", )
+
+    cursor.fetchall.assert_called_once()
+
+    cursor.executemany.assert_called_once()
+    assert len(cursor.executemany.call_args.args) == 2
+    assert canonicalize(
+            '''INSERT INTO "tgt_sch"."tgt_rel" ("foo_column") VALUES (%s) ON CONFLICT ("mock_id") DO NOTHING;'''
+        ) == canonicalize(
+            cursor.executemany.call_args.args[0]
+        )
+    assert cursor.executemany.call_args.args[1] == data
+
+
+def test_log_upload():
+    cursor = get_mock_cursor()
+    cursor.fetchone=MagicMock(return_value=[123])
+
+    ctx = mock_connection(cursor)
+
+    result = etl.log_upload(
+        etl.Relation("foo", "bar", "baz"),
+        etl.Relation("test", "public", "log_mock"),
+        "mock_datetime"
+    )
+
+    ctx.__enter__.assert_called_once()
+
+    etl.connect.assert_called_once_with("test")
+    assert len(cursor.execute.call_args_list) == 2
+
+    delete_call, insert_call = cursor.execute.call_args_list
+
+    assert len(delete_call.args) == 2
+    assert len(insert_call.args) == 2
+
+    assert canonicalize(
+            '''DELETE FROM "public"."log_mock" WHERE schema_name=%s AND relation_name=%s;'''
+        ) == canonicalize(
+            delete_call.args[0]
+        )
+    assert delete_call.args[1] == ('bar', 'baz')
+    
+    assert canonicalize(
+            '''INSERT INTO "public"."log_mock" (schema_name, relation_name, loaded) VALUES (%s, %s, %s);'''
+        ) == canonicalize(
+            insert_call.args[0]
+        )
+    assert insert_call.args[1] == ('bar', 'baz', 'mock_datetime')
